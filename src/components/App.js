c8ba9a9a4442f02388326c6f280136ae666a89c6
loic.k <loickenmoe1@gmail.com>
2024-10-30 16:14:03 +0100

    My twice firsth react component

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import '../styles/app.css';
+import Header from "../components/Header"
+// import Accordian from './accordian';
+import RandomColor from './random-color';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* accordian component */}
+      {/* <Accordian /> */}
+
+      {/* random color component */}
+      <RandomColor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/data.js
@@ -0,0 +1,25 @@
+
+const data = [
+    {
+        id : '1',
+        question: "What are accordion components ?",
+        answer: "Accordion components are user interface element used for organize",
+    },
+    {
+        id : '2',
+        question: "What are they used for ?",
+        answer: "They are comonly employed in various contexts, including FAQS, product description",
+    },
+    {
+        id : '3',
+        question: "Accordion as a musical intrument",
+        answer: "The accordion is a musical instrument with a keyboard and bellows. It produces sound ",
+    },
+    {
+        id : '4',
+        question: "Can I create an accordion component with a different fr ?",
+        answer: "Yes of course, it's very possible to create an accodion component with another fr",
+    },
+];
+
+export default data;
