f79256f426ae534fcecd356dee03fb845ce00a74
loic.k <loickenmoe1@gmail.com>
2024-10-31 14:29:34 +0100

    My fouth react component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import '../styles/app.css';
-import Header from "../components/Header"
+import "../styles/app.css";
+import Header from "../components/Header";
+import ImageSlider from "./image-slider";
 // import Accordian from './accordian';
 // import RandomColor from './random-color';
-import StarRating from './star-rating';
-
+// import StarRating from './star-rating';
 
 function App() {
   return (
@@ -16,7 +16,13 @@ function App() {
       {/* <RandomColor /> */}
 
       {/* start rating component */}
-      <StarRating noOfStarts={11} />
+      {/* <StarRating noOfStarts={11} /> */}
+
+      {/* image slider component */}
+      <ImageSlider 
+      url={"https://picsum.photos/v2/list"}
+      page={"1"}
+      limit={"10"} />
     </div>
   );
 }
