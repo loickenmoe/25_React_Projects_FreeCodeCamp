96ab8b0021e391ab2a86fbb8c94855e01da1db64
loic.k <loickenmoe1@gmail.com>
2024-10-31 11:13:23 +0100

    My thirth component: star-rating

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import '../styles/app.css';
 import Header from "../components/Header"
 // import Accordian from './accordian';
-import RandomColor from './random-color';
+// import RandomColor from './random-color';
+import StarRating from './star-rating';
 
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
       {/* <Accordian /> */}
 
       {/* random color component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      {/* start rating component */}
+      <StarRating noOfStarts={11} />
     </div>
   );
 }
